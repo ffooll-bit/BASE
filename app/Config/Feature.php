6cc3d1953ae183dd736102e5079a4072a9da5319
Ammar Fathin <muhammadammarfathin@gmail.com>
2026-07-27 11:52:12 +0800

    chore: clean up leftover files and config after CI4 4.7.4 / AdminLTE 4 migration

--- app/Config/Feature.php
+++ app/Config/Feature.php
@@ -12,7 +12,7 @@ class Feature extends BaseConfig
     /**
      * Use improved new auto routing instead of the default legacy version.
      */
-    public bool $autoRoutesImproved = false;
+    public bool $autoRoutesImproved = true;
 
     /**
      * Use filter execution order in 4.4 or before.
@@ -25,5 +25,5 @@ class Feature extends BaseConfig
      * If true, `limit(0)` returns all records. (the behavior of 4.4.x or before in version 4.x.)
      * If false, `limit(0)` returns no records. (the behavior of 3.1.9 or later in version 3.x.)
      */
-    public bool $limitZeroAsAll = true;
+    public bool $limitZeroAsAll = false;
 }
